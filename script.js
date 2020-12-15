fffcfa99df73a5d903c522cca92fb1974f87f893
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-12-15 14:22:33 +0100

    feature/calendar:git init

---
+++ script.js
@@ -0,0 +1,11 @@
+/* mettre en avant  la date du jour 
+Le mois (avec un code couleur)
+afficher quand le parc est ouvert ou fermer 
+fermer tous les premiers mardis de chaque mois 
+Journées prolongées – Nocturnes (tous les vendredis et les samedis)
+Journées spéciales (tous les derniers dimanches de chaque mois)
+les horaires d’ouverture et de fermeture du jour.
+Du lundi au jeudi : 9h-19h 
+Du vendredi au samedi : 9h-20h
+Le dimanche : 9h – 18h
+*/
