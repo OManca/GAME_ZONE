73a426c8b667f920cb0b4bea8f9b5f85b34e8b9f
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-12-15 13:41:02 +0100

    feature/rate_calculator:addeventlistener to input

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let tarifone = 12.50;
 let tariftwo = 13.50;
 let tariftree = 15;
-let devis = 0;
-let numbers = [];
-let sumPeople = 0;
-
-let inputs = document.querySelectorAll('.number')
-for (let i = 0; i < inputs.length ; i++) {
-	numbers.push(inputs[i].value)
-	sumPeople += parseInt(inputs[i].value)
-};
-
-let sumPepole = document.getElementById('sumPepole');
-sumPepole.innerHTML = sumPeople;
-
-devis = (numbers[1]*tarifone)+(numbers[2]*tariftwo)+(numbers[3]*tariftree);
+let inputs = document.querySelectorAll('.number');
+let sumPepoles = document.getElementById('sumPepole');
 let sumTotal = document.querySelector('.sumTotal');
-sumTotal.innerHTML = devis
+
+for (let y = 0; y < inputs.length ; y++) {
+	inputs[y].addEventListener( 'input', event => {
+		let sumPeople = 0;
+		
+		let numbers = [];
+		for (let i = 0; i < inputs.length ; i++) {
+			numbers.push(inputs[i].value)
+			sumPeople += parseInt(inputs[i].value)
+		};
+		
+		sumPepoles.innerHTML = sumPeople;
+		let devis = 0
+		devis += (numbers[1]*tarifone)+(numbers[2]*tariftwo)+(numbers[3]*tariftree);
+		sumTotal.innerHTML = devis
+		console.log(devis);
+	});
+};
