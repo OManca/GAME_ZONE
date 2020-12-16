147534ea0cc9dc78adea778e8db7860fd4b770b3
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-12-16 16:37:59 +0100

    feature/calendar:add full calendar

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-/* mettre en avant  la date du jour 
+let date = new Date()
+// let dateTest = new Date(date.getFullYear(), date.getMonth() +1, 0).getDate()
+let dateTest = new Date(2020, 2, 0).getDate()
+let today = document.getElementById('today')
+let calendar = document.querySelector('#app-calendar');
+let years = document.getElementById('selectYear')
+
+console.log();
+const selectElement = document.querySelector('.ice-cream');
+
+//Pour la date du jour 
+today.insertAdjacentHTML("beforeend",`<h2> ${(date.getDate()+"/"+(date.getMonth())+"/"+date.getFullYear())}</h2>`);
+
+let LastSunday = day => {
+    return day % 28 === 0
+}
+let FirstTuesday = day => {
+    return day % 30 === 2
+}
+let nocturnes = day => {
+    return day % 7 === 5 || day % 7 ===6 
+}
+//Pour selectionneer les chiffre du weenkend et les mettre en couleur 
+let weekends = day => {
+    // le 6 pour la ligne samedi ou le 0 pour la ligne dimache 
+    return day % 7 ===6 || day % 7 === 0
+}
+
+
+//Je crée une boulce pour chaque jour du calendirier 
+for (let day = 1; day <= dateTest; day++) {
+    let first =  FirstTuesday(day)
+    let last = LastSunday(day)
+    let nocturne = nocturnes(day)
+    let weekend = weekends(day)
+    // Pour placer les jour de la semaine dans les cases 
+    let dayName = day => {
+        let date = new Date(Date.UTC(2018, 0, day));
+        return new Intl.DateTimeFormat("fr", {weekday: "short"})
+        .format(date);
+    }
+    
+    calendar.insertAdjacentHTML("beforeend", `<div class= "day ${weekend ? "weekend" : ""} ${nocturne ? "nocturne" : ""} ${first ? "FirstTuesday" : ""} ${last ? "LastSunday" : ""} "><div class="name">${dayName(day)}</div>${day}</day>`) 
+}
+
+// ${nocturne ? "nocturene" : ""}
+/* 
 Le mois (avec un code couleur)
-afficher quand le parc est ouvert ou fermer 
 fermer tous les premiers mardis de chaque mois 
-Journées prolongées – Nocturnes (tous les vendredis et les samedis)
 Journées spéciales (tous les derniers dimanches de chaque mois)
-les horaires d’ouverture et de fermeture du jour.
-Du lundi au jeudi : 9h-19h 
-Du vendredi au samedi : 9h-20h
-Le dimanche : 9h – 18h
 */
