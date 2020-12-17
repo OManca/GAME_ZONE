4344c03a04a6fe89abb72a5d3890cf5d422b2791
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-12-17 13:51:06 +0100

    Add leaflet routing machine and leaflet control geocoder

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-let map = L.map('map').setView([40.91, -96.63], 4);
+// let map = L.map('map').setView([48.871900, 2.776623], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// 	attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
-let searchControl = L.esri.Geocoding.geosearch().addTo(map);
+// let searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
-let results = L.layerGroup().addTo(map);
+// let results = L.layerGroup().addTo(map);
 
-searchControl.on('results', function (data) {
-	results.clearLayers();
-	for (let i = data.results.length - 1; i >= 0; i--) {
-		results.addLayer(L.marker(data.results[i].latlng));
-	}
-});
+// searchControl.on('results', function (data) {
+// 	results.clearLayers();
+// 	for (let i = data.results.length - 1; i >= 0; i--) {
+// 		results.addLayer(L.marker(data.results[i].latlng));
+// 	}
+// });
+
+window.onload = function(){
+	let carte = L.map("carte").setView([48.852969, 2.349903],13)
+	L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',).addTo(carte)
+
+	L.routing.control({
+		geocoder: L.Control.Geocoder.nominatim(),
+		lineOptions: {
+			styles: [{
+				color: '#C9116A',
+				opacity: 1,
+			}]
+		},
+		router: new L.Routing.osrmv1({
+			language: 'fr',
+			profil: 'velo'
+		})
+	}).addTo(carte)
+}
