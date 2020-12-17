7ee4e67914cc86a68a753d9f521ddfe8300fc859
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-12-17 15:14:43 +0100

    feature/go_to_the_park: clean the code

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// let map = L.map('map').setView([48.871900, 2.776623], 13);
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-// 	attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// let searchControl = L.esri.Geocoding.geosearch().addTo(map);
-
-// let results = L.layerGroup().addTo(map);
-
-// searchControl.on('results', function (data) {
-// 	results.clearLayers();
-// 	for (let i = data.results.length - 1; i >= 0; i--) {
-// 		results.addLayer(L.marker(data.results[i].latlng));
-// 	}
-// });
-
-
 navigator.geolocation.getCurrentPosition(showPosition);
 
 let longitude = 0;
@@ -27,7 +9,6 @@ function showPosition(position) {
 
 	let carte = L.map("carte").setView([48.852969, 2.349903],13)
 	L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',).addTo(carte)
-	console.log(latitude);
 	L.routing.control({
 		waypoints: [
 			L.latLng(latitude, longitude ),
@@ -42,7 +23,7 @@ function showPosition(position) {
 		},
 		router: new L.Routing.osrmv1({
 			language: 'fr',
-			profil: 'velo'
+			profil: 'bike'
 		})
 	}).addTo(carte)
 }
