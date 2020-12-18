a29437495079156d776cc68785b91f2c45b1818e
ophelie <ophelie.manca@gmails.com>
2020-12-18 10:23:15 +0100

    conflict same name file "index"

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let inputs = document.querySelectorAll('.input');
 let images = document.querySelectorAll('.check');
 
@@ -11,3 +12,24 @@ for(let i = 0; i < inputs.length; i++) {
     })
 }
 
+=======
+let tarifone = 12.50;
+let tariftwo = 13.50;
+let tariftree = 15;
+let devis = 0;
+let numbers = [];
+let sumPeople = 0;
+
+let inputs = document.querySelectorAll('.number')
+for (let i = 0; i < inputs.length ; i++) {
+	numbers.push(inputs[i].value)
+	sumPeople += parseInt(inputs[i].value)
+};
+
+let sumPepole = document.getElementById('sumPepole');
+sumPepole.innerHTML = sumPeople;
+
+devis = (numbers[1]*tarifone)+(numbers[2]*tariftwo)+(numbers[3]*tariftree);
+let sumTotal = document.querySelector('.sumTotal');
+sumTotal.innerHTML = devis
+>>>>>>> feature/rate_calculator:calculator people and total
